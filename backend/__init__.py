9cff97b36c799c40f61f32c13fc9db65d4edaa3f
Kristian Cross <etamin1985@yahoo.com>
2026-06-26 16:16:08 +0100

    Add docstring to backend/__init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,6 @@
+"""
+FastAPI backend package for the Grimsby-London simulation.
+This package contains the FastAPI application and its associated modules for handling requests,
+processing data,
+and serving responses related to the Grimsby-London simulation.
+"""
